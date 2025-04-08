76633ade2f811e862b3f5954ac5b8f37e68c011f
cesar-martinez-torres <cesar.martinez@udlap.mx>
2025-04-08 17:34:54 -0600

    8 abril

--- Codigos_clase/Teoria/abr_8.m
+++ Codigos_clase/Teoria/abr_8.m
@@ -1,13 +1,19 @@
-%% Robot Diap
+%% Robot SCARA
 clear 
 close all
 % Cargar línea a línea la tabla de parámetros DH
 % theta d a alpha tipo de articulación
-
-L(1)= Link([0 l1 0 0 0]); %Generar cada uno de los eslabones
-L(2)= Link([pi/2 0 0 pi/2 1]);
-L(2).qlim=[0 d2]; % Las articulaciones primsmaticas necesitan limites fisicos
+l1=4
+l2=4
+l3=3
+l5=5
+q3m=2.5
+L(1)= Link([0 0 l1 0 0]); %Generar cada uno de los eslabones
+L(2)= Link([0 0 l2 pi 0]);
 L(3)= Link([0 0 0 0 1]);
-L(3).qlim=[0 d3];
-L(4)=Link([0 l4 0 0 0]);
-cilin= SerialLink(L, 'name', 'Cilindrico'); % Armar el robot uniendo los eslabones
+L(3).qlim=[0 q3m];
+L(4)=Link([0 l3 0 0 0]);
+scara= SerialLink(L, 'name', 'SCARA'); % Armar el robot uniendo los eslabones
+scara.base=transl(0,0,l5)
+figure
+scara.teach
