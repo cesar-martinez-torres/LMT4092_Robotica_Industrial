a99807f223c96d271ebca31efa3cd3293d328378
cesar-martinez-torres <cesar.martinez@udlap.mx>
2025-04-08 18:07:06 -0600

    8 abril

--- Codigos_clase/Teoria/abr_8.m
+++ Codigos_clase/Teoria/abr_8.m
@@ -17,3 +17,15 @@ scara= SerialLink(L, 'name', 'SCARA'); % Armar el robot uniendo los eslabones
 scara.base=transl(0,0,l5)
 figure
 scara.teach
+clear 
+close all
+%% Robot Diap 37
+l1=5
+q3m=4
+L(1)= Link([0 l1 0 pi/2 0]); %Generar cada uno de los eslabones
+L(2)= Link([0 0 0 -pi/2 0]);
+L(3)= Link([0 0 0 0 1]);
+L(3).qlim=[0 q3m];
+diap37= SerialLink(L, 'name', 'DIAP 37'); % Armar el robot uniendo los eslabones
+figure
+diap37.teach
